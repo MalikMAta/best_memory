185b8edaf27fc10d4437b32744802650e6c83043
Malik Ata <Malikmajedata@gmail.com>
2021-02-25 14:44:32 -0800

    Server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,25 @@
-import expres from 'express'
+import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+
+
+// intitiliaze the app
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors())
+
+const CONNECTION_URL ="mongodb+srv://Malik:Memoryapp1!@cluster0.6k5df.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 500;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+app.listen(PORT, () => console.log(`Server is running on port ${PORT} `)) 
+
+}).catch((error) => {
+        console.log(error.message(error.message) )
+})
+
+mongoose.set('useFindAndModify', false)
